b622800b4ca6ce5653563f809c650a54adcbf9d2
JuliaSzczygiel <62343530+JuliaSzczygiel@users.noreply.github.com>
2021-04-29 12:16:59 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+int main(int argc, char argv)
+{
+    ifstream plik;
+    plik.open("plik.txt");
+
+    char licz = 0;
+
+    for (int i = 2; i < argc; i++) {
+        plik >> argv;
+    }
+
+    if (argc == 4) 
+	{
+    	for (int i = 0; i < 4; i++) 
+		{
+            if(licz>argv) 
+			{
+                licz = argv;
+            }
+        }
+    }
+
+    cout << "Najwieksza liczba to: "<<licz <<endl;
+    return 0;
+}
